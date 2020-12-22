8ec32e47df9c397431462cfd2e174f129fb692a4
Temur <tima000101@gmail.com>
2020-12-23 00:13:22 +0600

    Connected Vuex & Added requests

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    companyInfo: [],
+    cities: []
+  },
+  mutations: {
+    getCompanyInfo: (state, companyInfo) => (state.companyInfo = companyInfo)
+  },
+  actions: {
+    async fetchCompanyInfo({ commit }) {
+      const res = await axios.get(
+        "https://bitbucket.org/ilakhmotkin/front-end-assesment-ru/raw/525e8e2fbee5cd98a80499c400f569e77b4f6ffd/server-response.json"
+      );
+      commit("getCompanyInfo", res.data);
+    }
+  },
+  getters: {
+    allCities(state) {
+      // return state.cities;
+      return state.companyInfo;
+    }
+  }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-row justify="center">
+      <v-btn color="primary" class="ma-2" dark @click="dialog = true">
+        Open Dialog
+      </v-btn>
+    </v-row>
+    <v-dialog
+      class="d-flex justify-start pa-lg-15"
+      v-model="dialog"
+      max-width="1000px"
+    >
+      <v-card>
+        <v-row class="py-5 px-10 ma-0">
+          <v-col>
+            <v-card elevation="2">
+              <v-card-title class="headline">
+                Данные о компании
+              </v-card-title>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="allCities.name"
+                    counter="50"
+                    label="Название компании"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    type="number"
+                    v-model="allCities.status.totalProducts"
+                    counter="50"
+                    label="Общее количество товаров"
+                    required
+                  ></v-text-field>
+                  <v-autocomplete
+                    ref="city"
+                    v-model="city"
+                    :rules="[() => !!city || 'This field is required']"
+                    :items="allCities.cities"
+                    label="Город"
+                    placeholder="Выбрать город..."
+                    required
+                  ></v-autocomplete>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col></v-col>
+        </v-row>
+        <v-card-actions class="d-flex justify-end">
+          <v-btn color="primary" outlined @click="dialog = false">
+            Отменить
+          </v-btn>
+          <v-btn color="primary" @click="dialog = false">
+            Сохранить
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      company: [],
+      dialog: false,
+      city: null
+    };
+  },
+  mounted() {
+    this.fetchCompanyInfo();
+  },
+  computed: {
+    ...mapGetters(["allCities"])
+  },
+  methods: {
+    ...mapActions(["fetchCompanyInfo"])
   }
 };
 </script>
